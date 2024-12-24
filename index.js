84a5eea28dd0d5bb080a9041a5b7fad46a36b660
TejasBhosale123 <108449510+TejasBhosale123@users.noreply.github.com>
2024-12-24 13:05:00 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+app.use(cors());
+
+app.get('/check' , (req,res)=>{
+    let n = parseInt(req.query.number);
+    let r = n % 2 === 0 ? "Even" : "Odd";
+    res.json({"msg":r})
+})
+
+
+
+app.listen(9000, () =>{
+    console.log('Server running on port @ 9000');
+})
